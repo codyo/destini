bf59fc79a6850d03b2cb0dff28537421f4238c45
Cody Tolmasoff <codytolmasoff@gmail.com>
2018-10-06 22:47:55 -0700

    app set up and working

--- Destini-iOS11-master/Destini/ViewController.swift
+++ Destini-iOS11-master/Destini/ViewController.swift
@@ -33,8 +33,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var topButton: UIButton!         // Has TAG = 1
     @IBOutlet weak var bottomButton: UIButton!      // Has TAG = 2
     @IBOutlet weak var storyTextView: UILabel!
+    @IBOutlet weak var restartButtin: UIButton!
     
     // TODO Step 5: Initialise instance variables here
+    var selectedAnswer:String = "a"
+    var storyIndex:Int = 1
     
     
     
@@ -42,9 +45,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
         // TODO Step 3: Set the text for the storyTextView, topButton, bottomButton, and to T1_Story, T1_Ans1, and T1_Ans2
-        
+        storyTextView.text = story1
+        topButton.setTitle(answer1a, for: .normal)
+        bottomButton.setTitle(answer1b, for: .normal)
+        restartButtin.isHidden = true
     }
 
     
@@ -52,13 +57,91 @@ class ViewController: UIViewController {
     @IBAction func buttonPressed(_ sender: UIButton) {
     
         // TODO Step 4: Write an IF-Statement to update the views
+        if(sender.tag == 1){
+            selectedAnswer = "a"
+        } else {
+            selectedAnswer = "b"
+        }
+        
+        navigateStory()
+        updateStory()
                 
         // TODO Step 6: Modify the IF-Statement to complete the story
         
     
     }
     
-
+    func navigateStory() {
+        switch storyIndex {
+            case 1:
+                if selectedAnswer == "a" {
+                    storyIndex = 3
+                } else {
+                    storyIndex = 2
+                }
+            case 2:
+                if selectedAnswer == "a" {
+                    storyIndex = 3
+                } else {
+                    storyIndex = 4
+                }
+            case 3:
+                if selectedAnswer == "a" {
+                    storyIndex = 6
+                } else {
+                    storyIndex = 5
+                }
+            default:
+                print("navigateStoryDefault")
+            }
+    }
+    
+    func updateStory() {
+        storyTextView.text = story1
+        topButton.setTitle(answer1a, for: .normal)
+        bottomButton.setTitle(answer1b, for: .normal)
+        
+        switch storyIndex {
+        case 1:
+            storyTextView.text = story1
+            topButton.setTitle(answer1a, for: .normal)
+            bottomButton.setTitle(answer1b, for: .normal)
+        case 2:
+            storyTextView.text = story2
+            topButton.setTitle(answer2a, for: .normal)
+            bottomButton.setTitle(answer2b, for: .normal)
+        case 3:
+            storyTextView.text = story3
+            topButton.setTitle(answer3a, for: .normal)
+            bottomButton.setTitle(answer3b, for: .normal)
+        case 4:
+            storyTextView.text = story4
+            topButton.isHidden = true
+            bottomButton.isHidden = true
+            restartButtin.isHidden = false
+        case 5:
+            storyTextView.text = story5
+            topButton.isHidden = true
+            bottomButton.isHidden = true
+            restartButtin.isHidden = false
+        case 6:
+            storyTextView.text = story6
+            topButton.isHidden = true
+            bottomButton.isHidden = true
+            restartButtin.isHidden = false
+        default:
+            print("updateStoryDefault")
+        }
+    }
+    
+    @IBAction func restart(_ sender: UIButton) {
+        storyIndex = 1
+        updateStory()
+        topButton.isHidden = false
+        bottomButton.isHidden = false
+        restartButtin.isHidden = false
+    }
+    
 
 
 }
